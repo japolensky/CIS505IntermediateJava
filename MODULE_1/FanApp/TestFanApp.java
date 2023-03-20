be7d97d0a70c539efa40158b67a48ebf5a675e38
Jason <japolensky@my365.bellevue.edu>
2023-03-20 00:03:33 -0500

    update 1203monday

---
+++ MODULE_1/FanApp/TestFanApp.java
@@ -0,0 +1,12 @@
+
+
+//public class TestFanApp {
+    public static void main(String[] args) {  //main method
+
+// Create two Fan objects
+        Fan fanone = new Fan();
+        Fan fantwo = new Fan();
+		
+
+    }
+//}
