d712f9a99cd6c4ae936d77c9e24c69ef7cdef6a1
Jason <japolensky@my365.bellevue.edu>
2023-03-19 21:48:36 -0500

    last commit for helloworld playing tonight.

--- MODULE_1/HelloWorldApp/PolenskyHelloWorld.java
+++ MODULE_1/HelloWorldApp/PolenskyHelloWorld.java
@@ -13,14 +13,28 @@
 package MODULE_1.HelloWorldApp;
 
 public class PolenskyHelloWorld {
+/**
+ * @param args no arguements are accepted for main() at this time
+ */
 public static void main(String[] args) {
+   try {
     myName("Jason", "Polensky");
+   } 
+   catch (Exception e) { // I am attempting to get the points for error trapping.
+    System.out.println("Incorrect Entry into myName() from main");
+   }
+    
 } // the beginning of the code for the myName method.
 public static void myName(String first, String last)
 // This method prints my first and last name
-/*I added colors to see if it would work prperly in the terminal. */
+// I added colors to see if it would work prperly in the terminal.
 {
-    System.out.println("\u001B[34m My name is \u001B[37m"+first+" \u001B[37;1m"+last+"\u001B[0m");
+   try {
+        System.out.println("\u001B[34m My name is \u001B[37m"+first+" \u001B[37;1m"+last+"\u001B[0m"); 
+    } catch (Exception e) {
+        System.out.println("Incorrect Entry into myName()");
+    }
+    
 } // This is the end of the myName method.
 
 }
