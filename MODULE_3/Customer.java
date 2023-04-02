89291042e8dbb5d46c2a2924623dfe98d6e9ba91
Jason <japolensky@my365.bellevue.edu>
2023-04-02 14:12:33 -0500

    1st commit week3

---
+++ MODULE_3/Customer.java
@@ -0,0 +1,23 @@
+/* 
+
+(The Customer class #1). Design a class named Customer to represent a customer. This class contains:
+1. A private string data field named name that specifies the name of a customer.
+2. A private string data field named address that specifies the address of a customer.
+3. A private string data field named city that specifies the city of a customer.
+4. A private string data field named zip that specifies the zip code of a customer.
+5. A no-argument constructor that creates a default customer.
+6. An argument constructor that creates a customer using the four data fields.
+7. Accessor methods for all four data fields.
+8. Override the toString() method.
+
+*/
+
+class Customer{
+private String name;
+private String address;
+private String city;
+private String zip;
+
+
+
+}
